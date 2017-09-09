0775bccf945d668aad463fc4e1d1f9c6e964736b
Ian Agpawa <agpawaji@gmail.com>
2017-09-09 12:46:44 -0400

    refactor: Clean upjs code

--- popup.js
+++ popup.js
@@ -1,39 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     function getSearchResults(searchTerm) {
-
-        var searchURL = 'http://www.google.com/search?q=' + searchTerm;
+        var searchURL = "http://www.google.com/search?q=" + searchTerm;
         var xml = new XMLHttpRequest();
-        xml.responseType = 'document';
-
+        xml.responseType = "document";
         xml.onreadystatechange = function () {
             var xmlDoc = xml.response;
             var results = xmlDoc.getElementsByClassName("r");
             var display = "<h4 id='search-h4'>Search results for "+ searchTerm + "</h4><ol id='list'>";
+
             for (var i = 0; i < results.length; i++) {
-                var child = results[i].childNodes[0]
-                var text = results[i].childNodes[0].childNodes[0].nodeValue;
-                var link = results[i].childNodes[0].getAttribute('href')
+                var child = results[i].childNodes[0];
+                var text = child.childNodes[0].nodeValue;
+                var link = child.getAttribute("href");
+
                 if ( text !== null ) {
-                    display += "<li class='list-result'><a href='" + link + "' target='_blank'>" + text + "</a></li>"
-                    // display += text
+                    display += "<li class='list-result'><a href='" + link + "' target='_blank'>" + text + "</a></li>";
                 }
             }
-            display += "</ol>"
 
-            document.getElementById('results').innerHTML = display;
+            display += "</ol>";
+            document.getElementById("results").innerHTML = display;
         }
-
         xml.open("GET", searchURL, true);
         xml.send();
     }
 
 
-
-    document.getElementById("submit").addEventListener('click', function() {
-
-        searchTerm = document.getElementById('search');
-        // document.getElementById('results').innerHTML = searchTerm.value;
+    document.getElementById("submit").addEventListener("click", function () {
+        searchTerm = document.getElementById("search");
         getSearchResults(searchTerm.value);
     })
 });
